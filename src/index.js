c522ace8116e94efe838428a5ce9019df565ff56
Steve Kane <kanesteven@gmail.com>
2018-01-02 22:54:46 -0600

    now includes basic solver for distance constraints

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const Regl = require("regl")
 const { RenderDistanceConstraints, RenderPoints } = require("./rendering")
 
@@ -5,6 +7,7 @@ const regl = Regl()
 const renderDistanceConstraints = RenderDistanceConstraints(regl)
 const renderPoints = RenderPoints(regl)
 const rr = (min, max) => Math.random() * (max - min) + min
+const ITERATION_COUNT = 10
 const PARTICLE_COUNT = 4
 const DISTANCE_CONSTRAINT_COUNT = 4
 const G = -1
@@ -16,13 +19,11 @@ const distanceConstraintLines = new Float32Array(DISTANCE_CONSTRAINT_COUNT * 2 *
 
 for (var i = 0, o, t; i < PARTICLE_COUNT; i++) {
   o = i * 3
-  invmasses[i + 0] = rr(-1, 1)
+  invmasses[i + 0] = i / PARTICLE_COUNT
   t = i * Math.PI * 2 / PARTICLE_COUNT
-  // positions[o + 0] = rr(-1, 1)
-  // positions[o + 1] = rr(-1, 1)
-  // positions[o + 2] = rr(-1, 1)
-  positions[o + 0] = Math.cos(t) * .8
-  positions[o + 1] = Math.sin(t) * .8
+  positions[o + 0] = Math.sin(t) * .8
+  positions[o + 1] = Math.cos(t) * .8
+  positions[o + 2] = 0
 }
 
 const invmassbuffer = regl.buffer({ 
@@ -38,8 +39,10 @@ const distanceConstraintBuffer = regl.buffer({
   usage: "dynamic"
 })
 
-function applyExternalForces(dT, ps, vs) {
+function applyExternalForces(dT, ws, ps, vs) {
   for (var i = 0; i < PARTICLE_COUNT; i++) {
+    if (ws[i] == 0)
+      continue
     vs[i * 3 + 1] += dT * G // GRAVITY
   }
 }
@@ -74,10 +77,71 @@ function updatePositions(estimates, positions) {
   positions.set(estimates) 
 }
 
-function projectConstraints(iterations, estimates, pcs) {
-  while (iterations--) {
-    console.log(pcs)
+function projectConstraints(iterations, estimates, ws, pcs) {
+  var lambda = 1 / iterations
+  var l = pcs.length
+  var i = 0
+  var c, d
+  var i1, i2
+  var x1, y1, z1
+  var x2, y2, z2
+  var dx, dy, dz
+  var dp1, dp2
+  var dist, distdiff, dirx, diry, dirz
+  var w1, w2, wsum
+  var dp1x, dp1y, dp1z
+  var dp2x, dp2y, dp2z
+  var w1, w2
+
+  while (iterations-- > 0) {
+    while (i < l) {
+      c = pcs[i++]
+      d = c.d
+      w1 = ws[c.i1]
+      w2 = ws[c.i2]
+      i1 = c.i1 * 3
+      i2 = c.i2 * 3
+      x1 = estimates[i1++]
+      y1 = estimates[i1++]
+      z1 = estimates[i1]
+      x2 = estimates[i2++]
+      y2 = estimates[i2++]
+      z2 = estimates[i2]
+      dx = x1 - x2
+      dy = y1 - y2
+      dz = z1 - z2
+      dist = Math.sqrt(dx * dx + dy * dy + dz * dz) - d
+      distdiff = dist - d
+      dirx = dx / dist
+      diry = dy / dist
+      dirz = dz / dist
+      wsum = w1 + w2
+      // some redundant calculation here... could be refactored
+      dp1x = lambda * -w1 * distdiff * dirx / wsum
+      dp1y = lambda * -w1 * distdiff * diry / wsum
+      dp1z = lambda * -w1 * distdiff * dirz / wsum
+      dp2x = lambda * w2 * distdiff * dirx / wsum
+      dp2y = lambda * w2 * distdiff * diry / wsum
+      dp2z = lambda * w2 * distdiff * dirz / wsum
+      estimates[i1--] = dp1z + z1
+      estimates[i1--] = dp1y + y1
+      estimates[i1]   = dp1x + x1
+      estimates[i2--] = dp2z + z2
+      estimates[i2--] = dp2y + y2
+      estimates[i2]   = dp2x + x2
+    }
   }
+}
+
+function distance(x1, y1, z1, x2, y2, z2) {
+  var dx = x1 - x2
+  var dy = y1 - y2
+  var dz = z1 - z2
+
+  dx *= dx
+  dy *= dy
+  dz *= dz
+  return Math.sqrt(dx + dy + dz)
 }
 
 function updateDistanceConstraintLines(ps, cs, cls) {
@@ -86,13 +150,12 @@ function updateDistanceConstraintLines(ps, cs, cls) {
   var o = 0
   var c
   var i1, i2
-  var p1x, p1y, p1z
-  var p2x, p2y, p2z
 
   while (i < l) {
     c = cs[i++]
     i1 = c.i1 * 3
     i2 = c.i2 * 3
+    cls.subarray
     cls[o++] = ps[i1++]
     cls[o++] = ps[i1++]
     cls[o++] = ps[i1++]
@@ -118,10 +181,10 @@ var distanceConstraintProps = {
   positions: distanceConstraintBuffer
 }
 var distanceConstraints = [
-  { i1: 0, i2: 1, d: .4 },
-  { i1: 1, i2: 2, d: .4 },
-  { i1: 2, i2: 3, d: .4 },
-  { i1: 3, i2: 0, d: .4 }
+  { i1: 0, i2: 1, d: .1 },
+  { i1: 1, i2: 2, d: .1 },
+  { i1: 2, i2: 3, d: .1 },
+  // { i1: 3, i2: 0, d: .1 }
 ]
 
 setTimeout(function () {
@@ -129,19 +192,22 @@ setTimeout(function () {
     then = now
     now = performance.now()
     dT = (now - then) * .001
-    applyExternalForces(dT, positions, velocities)
-    // damp velocities?
+    applyExternalForces(dT, invmasses, positions, velocities)
     estimatePositions(dT, estimates, positions, velocities)
-    // generate collision constraints
-    // iterate/project constraints
+    projectConstraints(ITERATION_COUNT, estimates, invmasses, distanceConstraints)
     updateVelocities(dT, estimates, positions, velocities)
     updatePositions(estimates, positions)
 
     count = updateDistanceConstraintLines(positions, distanceConstraints, distanceConstraintLines)
 
-    distanceConstraintProps.count = count
+    // update contents of buffers
     distanceConstraintBuffer.subdata(distanceConstraintLines)
     positionbuffer.subdata(positions)
+
+    // update properties for rendering
+    distanceConstraintProps.count = count
+
+    // render points and constraints
     renderPoints(particleProps)
     renderDistanceConstraints(distanceConstraintProps)
   })
@@ -149,5 +215,3 @@ setTimeout(function () {
 
 window.dcls = distanceConstraintLines
 window.positions = positions
-
-console.log(positions)
