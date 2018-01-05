b97a07527b13187cd7a3cf03e4bd71fd49a1f10f
Steve Kane <kanesteven@gmail.com>
2018-01-05 17:53:52 -0600

    simplified rendering a bit. changed scene to include static triangle and pendulum

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 "use strict"
 
 const Regl = require("regl")
-const { RenderDistanceConstraints, RenderPoints } = require("./rendering")
+const Camera = require("regl-camera")
+const rti = require("ray-triangle-intersection")
+const wtrti = require("ray-triangle-intersection")
+const V3 = require("gl-vec3")
+const { Render } = require("./rendering")
 
 const regl = Regl()
-const renderDistanceConstraints = RenderDistanceConstraints(regl)
-const renderPoints = RenderPoints(regl)
-const rr = (min, max) => Math.random() * (max - min) + min
-const rrint = (min, max) => Math.floor(rr(min, max))
+const camera = Camera(regl, {
+  distance: 4,
+  theta: 3 * Math.PI / 2
+})
+const render = Render(regl)
 const ITERATION_COUNT = 10
 const PARTICLE_COUNT = 4
 const DISTANCE_CONSTRAINT_COUNT = PARTICLE_COUNT
 const G = -10
-const SPREAD = .5
 const invmasses = new Float32Array(PARTICLE_COUNT)
 const velocities = new Float32Array(PARTICLE_COUNT * 3)
-// TODO: Could create one larger buffer. Would result in allocation all being contiguous
 const positions = [
   new Float32Array(PARTICLE_COUNT * 3),
   new Float32Array(PARTICLE_COUNT * 3)
@@ -23,13 +26,10 @@ const positions = [
 const distanceConstraintLines = new Float32Array(DISTANCE_CONSTRAINT_COUNT * 2 * 3)
 const distanceConstraints = []
 
-// This system is a hanging triangle swinging down from the left
-// and a single suspended point swinging down from the right. 
-// The goal of this system is to prevent intersections in a testable
-// environment
+const SPREAD = 1
 const p1 = [ 0, SPREAD, 0 ]
-const p2 = [ -SPREAD, SPREAD, SPREAD ]
-const p3 = [ -SPREAD, SPREAD, -SPREAD ]
+const p2 = [ 0, 0, SPREAD ] //[ -SPREAD, SPREAD, SPREAD ]
+const p3 = [ 0, 0, -SPREAD ] // [ -SPREAD, SPREAD, -SPREAD ]
 const p4 = [ SPREAD, SPREAD, 0 ]
 const LONG_D = Math.sqrt(SPREAD * SPREAD + SPREAD * SPREAD)
 
@@ -38,33 +38,15 @@ positions[0].set(p2, 3)
 positions[0].set(p3, 6)
 positions[0].set(p4, 9)
 
+invmasses[0] = 0
+invmasses[1] = .01 // .5
+invmasses[2] = .01 // .5
+invmasses[3] = 1
+
 distanceConstraints.push({ i1: 0, i2: 1, d: LONG_D, k: 1 })
 distanceConstraints.push({ i1: 1, i2: 2, d: SPREAD * 2, k: 1 })
 distanceConstraints.push({ i1: 2, i2: 0, d: LONG_D, k: 1 })
 distanceConstraints.push({ i1: 0, i2: 3, d: SPREAD, k: 1 })
-
-invmasses[0] = 0
-invmasses[1] = 1
-invmasses[2] = 1
-invmasses[3] = 1
-
-// for (var i = 0, o, t; i < PARTICLE_COUNT; i++) {
-//   o = i * 3
-//   invmasses[i + 0] = i / PARTICLE_COUNT
-//   t = i * Math.PI * 2 / PARTICLE_COUNT
-//   positions[0][o + 0] = Math.sin(t) * SPREAD
-//   positions[0][o + 1] = Math.cos(t) * SPREAD
-//   positions[0][o + 2] = rr(-1, 1)
-// }
-// 
-// for (var i = 0; i < PARTICLE_COUNT; i++) {
-//   distanceConstraints.push({ 
-//     i1: rrint(0, PARTICLE_COUNT), 
-//     i2: rrint(0, PARTICLE_COUNT), 
-//     d: rr(.7, .8), 
-//     k: rr(.8, .9)
-//   })
-// }
 
 const invmassbuffer = regl.buffer({ 
   data: invmasses, 
@@ -79,8 +61,7 @@ const distanceConstraintBuffer = regl.buffer({
   usage: "dynamic"
 })
 
-// apply some non-linear dampening and gravity
-function applyExternalForces(dt, ws, ps, vs) {
+function applyExternalForces(dt, ws, vs) {
   const DAMPING_FACTOR = .99
   const GDT = G * dt
 
@@ -201,13 +182,22 @@ const MAX_TIME = .1
 const INIT_TIME = 200
 const particleProps = {
   positions: positionbuffer,
-  inverseMasses: invmassbuffer,
   count: PARTICLE_COUNT,
-  size: 10
+  size: 10,
+  color: [ 0, .2, .5, 1 ],
+  primitive: "points"
 }
 const distanceConstraintProps = {
+  positions: distanceConstraintBuffer,
   count: 0,
-  positions: distanceConstraintBuffer
+  color: [ .2, .4, .1, 1 ],
+  primitive: "lines"
+}
+const meshProps = {
+  positions: new Float32Array([ p1[0], p1[1], p1[2], p2[0], p2[1], p2[2], p3[0], p3[1], p3[2] ]),
+  count: 3,
+  color: [ .7, .7, .1, 1 ],
+  primitive: "triangles"
 }
 
 var currentTime = performance.now() * .001
@@ -217,6 +207,7 @@ var i = 1
 var ii = 0
 var tmp = i
 var count = 0
+var tri = [ p1, p2, p3 ]
 
 setTimeout(function () {
   regl.frame(function () {
@@ -224,27 +215,54 @@ setTimeout(function () {
     var frameTime = Math.min(newTime - currentTime, MAX_TIME)
 
     currentTime = newTime
-    acc = acc + frameTime
 
-    tmp = i
-    i = ii
-    ii = tmp
+    if (!window.paused) {
+      acc = acc + frameTime
+      // ii is always estimates. i is always positions
+      while (acc >= DT) {
+        tmp = i
+        i = ii
+        ii = tmp
+        applyExternalForces(DT, invmasses, velocities)
+        estimatePositions(DT, positions[ii], positions[i], velocities)
 
-    while (acc >= DT) {
-      applyExternalForces(DT, invmasses, positions, velocities)
-      estimatePositions(DT, positions[ii], positions[i], velocities)
-      projectConstraints(ITERATION_COUNT, positions[ii], invmasses, distanceConstraints)
-      updateVelocities(DT, positions[ii], positions[i], velocities)
-      count = updateDistanceConstraintLines(positions[i], distanceConstraints, distanceConstraintLines)
-      distanceConstraintBuffer.subdata(distanceConstraintLines)
-      positionbuffer.subdata(positions[i])
-      distanceConstraintProps.count = count
-      t += DT
-      acc -= DT
+        var predicted = positions[ii].subarray(9, 12)
+        var point = positions[i].subarray(9, 12)
+        var p1 = positions[ii].subarray(0, 3)
+        var p2 = positions[ii].subarray(3, 6)
+        var p3 = positions[ii].subarray(6, 9)
+        var dir = [ 0, 0, 0 ]
+        var contact = [ 0, 0, 0 ]
+        
+        V3.subtract(dir, predicted, point)
+        V3.normalize(dir, dir)
+
+        var collides = rti(contact, point, dir, tri)
+        var distanceMoved = V3.distance(point, predicted)
+        var toContact = collides ? V3.distance(contact, point) : Infinity
+
+        if (collides && toContact < distanceMoved) {
+          console.log("hit")
+        }
+
+        projectConstraints(ITERATION_COUNT, positions[ii], invmasses, distanceConstraints)
+        updateVelocities(DT, positions[ii], positions[i], velocities)
+        t += DT
+        acc -= DT
+      }
     }
-    renderPoints(particleProps)
-    renderDistanceConstraints(distanceConstraintProps)
+
+    count = updateDistanceConstraintLines(positions[ii], distanceConstraints, distanceConstraintLines)
+    distanceConstraintBuffer.subdata(distanceConstraintLines)
+    positionbuffer.subdata(positions[ii])
+    distanceConstraintProps.count = count
+    camera(function (c) {
+      render(particleProps)
+      render(meshProps)
+      render(distanceConstraintProps)
+    })
   })
 }, INIT_TIME)
 
+window.paused = false
 window.positions = positions
--- src/rendering.js
+++ src/rendering.js
@@ -1,69 +1,43 @@
-module.exports.RenderDistanceConstraints = RenderDistanceConstraints
-module.exports.RenderPoints = RenderPoints
+module.exports.Render = Render
 
-function RenderDistanceConstraints(regl) {
+function Render(regl) {
   return regl({
     vert: `
       precision mediump float;
 
       attribute vec3 position;
 
+      uniform mat4 view;
+      uniform mat4 projection;
+
+      uniform float size;
+      uniform vec4 color;
+
       void main () {
-        gl_Position = vec4(position, 1);
+        gl_PointSize = size;
+        gl_Position = projection * view * vec4(position, 1);
       }
     `,
     frag: `
       precision mediump float;
 
-      const vec4 color = vec4(.1, .2, .5, 1);
+      uniform vec4 color;
 
       void main () {
         gl_FragColor = color; 
       }      
     `,
+    cull: { 
+      enable: true 
+    },
+    uniforms: {
+      size: (c, p) => p.size || 10,
+      color: regl.prop("color")
+    },
     attributes: {
       position: regl.prop("positions") 
     },
     count: regl.prop("count"),
-    primitive: "lines"
-  })
-}
-
-function RenderPoints(regl) {
-  return regl({
-    vert: `
-      precision mediump float; 
-
-      attribute vec3 position;
-      attribute float inverse_mass;
-
-      uniform float size;
-
-      varying vec4 color;
-
-      void main () {
-        color = vec4(inverse_mass, .2, .5, 1);
-        gl_PointSize = size;
-        gl_Position = vec4(position, 1);
-      }
-    `,
-    frag: `
-      precision mediump float;
-
-      varying vec4 color;
-
-      void main () {
-        gl_FragColor = color;
-      }
-    `,
-    uniforms: {
-      size: regl.prop("size") 
-    },
-    attributes: {
-      inverse_mass: regl.prop("inverseMasses"),
-      position: regl.prop("positions")
-    },
-    count: regl.prop("count"),
-    primitive: "points"
+    primitive: regl.prop("primitive")
   })
 }
